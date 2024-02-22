6ab96a420331a5ce4b6e37fccf7f431309a0bc8b
camimayer <camicatmayer@gmail.com>
2024-02-22 12:39:31 -0500

    implement part 1

---
+++ src/view/ImagePanel.java
@@ -0,0 +1,27 @@
+package view;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class ImagePanel extends JPanel {
+    private Image backgroundImage;
+
+    public ImagePanel() {
+        try {
+            backgroundImage = ImageIO.read(new File("src/assets/car.jpeg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (backgroundImage != null) {
+            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
