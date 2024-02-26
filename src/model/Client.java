b3671049a8af7728b96e2af46843f68ce824b142
Flavio Luz <frpluz@gmail.com>
2024-02-26 15:05:44 -0500

    Classes User, Client and Investor

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,4 +1,91 @@
 package model;
 
-public class Client {
+import java.time.LocalDate;
+
+public class Client extends User {
+
+    private String jobInfo;
+    private Double annualIncome;
+    private Integer creditNote;
+    private LocalDate birthDateField;
+    private String maritalStatus;
+    private Integer yearsInCanada;
+
+    public Client(String fullName, String email, String password, String phoneNumber, String jobInfo, Double annualIncome, Integer creditNote, LocalDate birthDateField, String maritalStatus, int yearsInCanada) {
+        super(fullName, email, password, phoneNumber);
+        this.jobInfo = jobInfo;
+        this.annualIncome = annualIncome;
+        this.creditNote = creditNote;
+        this.birthDateField = birthDateField;
+        this.maritalStatus = maritalStatus;
+
+        this.yearsInCanada = yearsInCanada;
+    }
+
+    public Client() {
+        super();
+    }
+
+    public String getJobInfo() {
+        return jobInfo;
+    }
+
+    public void setJobInfo(String jobInfo) {
+        this.jobInfo = jobInfo;
+    }
+
+    public Double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(Double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+
+    public Integer getCreditNote() {
+        return creditNote;
+    }
+
+    public void setCreditNote(Integer creditNote) {
+        this.creditNote = creditNote;
+    }
+
+    public LocalDate getBirthDateField() {
+        return birthDateField;
+    }
+
+    public void setBirthDateField(LocalDate birthDateField) {
+        this.birthDateField = birthDateField;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+
+    public Integer getYearsInCanada() {
+        return yearsInCanada;
+    }
+
+    public void setYearsInCanada(Integer yearsInCanada) {
+        this.yearsInCanada = yearsInCanada;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                ", fullName='" + getFullName() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", phoneNumber=" + getPhoneNumber() +
+                ", jobInfo='" + jobInfo + '\'' +
+                ", annualIncome=" + annualIncome +
+                ", creditNote=" + creditNote +
+                ", birthDateField=" + birthDateField +
+                ", maritalStatus='" + maritalStatus + '\'' +
+                ", yearsInCanada=" + yearsInCanada +
+                '}';
+    }
 }
--- src/model/Investor.java
+++ src/model/Investor.java
@@ -1,4 +1,54 @@
 package model;
 
-public class Investor {
+public class Investor extends User{
+
+    private String bankName;
+    private String accountDetails;
+    private String riskLevel;
+    private String levelEducation;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccountDetails() {
+        return accountDetails;
+    }
+
+    public void setAccountDetails(String accountDetails) {
+        this.accountDetails = accountDetails;
+    }
+
+    public String getRiskLevel() {
+        return riskLevel;
+    }
+
+    public void setRiskLevel(String riskLevel) {
+        this.riskLevel = riskLevel;
+    }
+
+    public String getLevelEducation() {
+        return levelEducation;
+    }
+
+    public void setLevelEducation(String levelEducation) {
+        this.levelEducation = levelEducation;
+    }
+
+    @Override
+    public String toString() {
+        return "Investor{" +
+                ", fullName='" + getFullName() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", phoneNumber=" + getPhoneNumber() +
+                ", bankName='" + bankName + '\'' +
+                ", accountDetails='" + accountDetails + '\'' +
+                ", riskLevel='" + riskLevel + '\'' +
+                ", levelEducation='" + levelEducation + '\'' +
+                '}';
+    }
 }
