556da3187627e673f766e465412f44d0f78ffa82
camimayer <camicatmayer@gmail.com>
2024-03-25 14:16:20 -0400

    att

--- src/view/InvestissementForm.java
+++ src/view/InvestissementForm.java
@@ -1,46 +1,80 @@
 package view;
-
 import DAO.InvestissementDAO;
 import DAO.InvestissementDAOImpl;
 import model.Investissement;
 import model.Investor;
+import view.InvestorView;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
+
 import static helper.Helper.dateConverter;
 import static javax.swing.JOptionPane.WARNING_MESSAGE;
 
 public class InvestissementForm extends JPanel {
     private JLabel amountLabel = new JLabel("Montant à investir: ");
     private JTextField amountField = new JTextField(10);
-    private JLabel bankInfoLabel = new JLabel("Nom de la banque: ");
+    private JLabel bankInfoLabel = new JLabel();
     private JTextField bankNameField = new JTextField(10);
     private JTextField transitNumberField = new JTextField(10);
     private JTextField institutionNumberField = new JTextField(10);
     private JTextField accountNumberField = new JTextField(10);
-    private JButton submitButton = new JButton("Submit");
-    private JButton backButton = new JButton("Page principal");
+    private JButton submitButton = new JButton("Soumettre");
+    private JButton backButton = new JButton("Retour à la page principale");
     int idInvestor = new Investor().getIdInvestor();
+
     public InvestissementForm(CardLayout cardLayout, JPanel cardPanel, JFrame main) {
-        setLayout(new GridLayout(0, 1));
-        JPanel amountPanel = new JPanel();
-        amountPanel.add(amountLabel);
-        amountPanel.add(amountField);
-        JPanel bankPanel = new JPanel();
-        bankPanel.add(bankInfoLabel);
-        bankPanel.add(bankNameField);
-        bankPanel.add(new JLabel("Numéro de Transit: "));
-        bankPanel.add(transitNumberField);
-        bankPanel.add(new JLabel("Numéro d'Institution: "));
-        bankPanel.add(institutionNumberField);
-        bankPanel.add(new JLabel("Numéro de compte: "));
-        bankPanel.add(accountNumberField);
-        JPanel buttonPanel = new JPanel();
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+
+        add(amountLabel, gbc);
+        gbc.gridx = 1;
+        add(amountField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        add(bankInfoLabel, gbc);
+
+        gbc.gridy = 2;
+        add(new JLabel("Nom de la banque: "), gbc);
+        gbc.gridx = 1;
+        add(bankNameField, gbc);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        add(new JLabel("Numéro de transit: "), gbc);
+        gbc.gridx = 1;
+        add(transitNumberField, gbc);
+
+        gbc.gridy = 4;
+        gbc.gridx = 0;
+        add(new JLabel("Numéro d'institution: "), gbc);
+        gbc.gridx = 1;
+        add(institutionNumberField, gbc);
+
+        gbc.gridy = 5;
+        gbc.gridx = 0;
+        add(new JLabel("Numéro de compte: "), gbc);
+        gbc.gridx = 1;
+        add(accountNumberField, gbc);
+
+        gbc.gridy = 6;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        add(submitButton, gbc);
+
+        gbc.gridy = 7;
+        add(backButton, gbc);
 
         submitButton.addActionListener(e -> {
-            if(validateInvestissement(amountField.getText())){
+            if (validateInvestissement(amountField.getText())) {
                 addInvestissementToDB();
                 cardLayout.show(cardPanel, "Main");
                 cardPanel.add(new InvestissementForm(cardLayout, cardPanel, main), "Investissement");
@@ -55,21 +89,15 @@ public class InvestissementForm extends JPanel {
                 cardPanel.add(new InvestorView(cardLayout, cardPanel, main), "Investor");
             }
         });
-
-        buttonPanel.add(submitButton);
-        buttonPanel.add(backButton);
-        add(amountPanel);
-        add(bankPanel);
-        add(buttonPanel);
     }
 
-    public void addInvestissementToDB(){
+    public void addInvestissementToDB() {
         Investissement investissement = prepareInvestissement();
         InvestissementDAO investissementDAO = new InvestissementDAOImpl();
         investissementDAO.addInvestissement(investissement);
     }
 
-    public Investissement prepareInvestissement(){
+    public Investissement prepareInvestissement() {
         Investissement investissement = new Investissement();
         investissement.setNomBanque(bankNameField.getText());
         investissement.setNumTransit(transitNumberField.getText());
@@ -78,25 +106,25 @@ public class InvestissementForm extends JPanel {
         if (!Objects.isNull(amountField.getText()) && !amountField.getText().isEmpty()) {
             investissement.setMontant(Double.valueOf(amountField.getText()));
         }
-        investissement.setIdInvestor(1);
+        investissement.setIdInvestor(idInvestor);
         investissement.setInvestissementDate(dateConverter("25/03/2024"));
         investissement.setInvestissementType("INVESTISSEMENT");
         return investissement;
     }
 
     public boolean validateInvestissement(String amount) {
-        if(amount.isEmpty()){
-            JOptionPane.showMessageDialog(null, "Vous devez remplir le champ Montant." , "Amount empty", WARNING_MESSAGE);
+        if (amount.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Vous devez remplir le champ Montant.", "Amount empty", WARNING_MESSAGE);
             return false;
-        }else{
-            try{
+        } else {
+            try {
                 double amountDouble = Double.valueOf(amount);
                 if (amountDouble < 100) {
-                    JOptionPane.showMessageDialog(null, "La valeur minimale d'investissement est de 100 $." , "Investissement", WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(null, "La valeur minimale d'investissement est de 100 $.", "Investissement", WARNING_MESSAGE);
                     return false;
                 }
-            }catch (Exception err){
-                JOptionPane.showMessageDialog(null, "La valeur saisie doit être numérique." , "Loan Amount", WARNING_MESSAGE);
+            } catch (Exception err) {
+                JOptionPane.showMessageDialog(null, "La valeur saisie doit être numérique.", "Loan Amount", WARNING_MESSAGE);
                 return false;
             }
         }
@@ -106,19 +134,25 @@ public class InvestissementForm extends JPanel {
     public JButton getSubmitButton() {
         return submitButton;
     }
+
     public JTextField getAmountField() {
         return amountField;
     }
+
     public JTextField getBankNameField() {
         return bankNameField;
     }
+
     public JTextField getTransitNumberField() {
         return transitNumberField;
     }
+
     public JTextField getInstitutionNumberField() {
         return institutionNumberField;
     }
+
     public JTextField getAccountNumberField() {
         return accountNumberField;
     }
 }
+
